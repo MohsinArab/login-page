f749b360081212827d2a29e21214cd960899b252
MoshanArab <moshanarab@gmail.com>
2021-03-08 19:36:19 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -18,92 +18,100 @@ class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[50],
         appBar: AppBar(
-          title: Text('Login Page'),
+          backgroundColor: Colors.pink[900],
+          title: Center(
+            child: Text('Binary Reactor'),
+          ),
         ),
-        body: Padding(
-            padding: EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/login.jpg'),
-            ),
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'BinaryReactor',
-                      style: TextStyle(
-                          color: Colors.indigo[200],
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30),
-                    )),
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
-                    )),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                    ),
-                  ),
+    
+        body: Container(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: ListView(
+                  children: <Widget>[
+                    CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage('images/login.jpg'),
                 ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                FlatButton(
-                  onPressed: (){
-                    //forgot password screen
-                  },
-                  textColor: Colors.blue,
-                  child: Text('Forgot Password'),
-                ),
-                Container(
-                  height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: RaisedButton(
-                      textColor: Colors.teal[200],
-                      color: Colors.blue,
-                      child: Text('Login'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-                Container(
-                  child: Row(
-                    children: <Widget>[
-                      Text('Does not have account?'),
-                      FlatButton(
-                        textColor: Colors.blue,
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
                         child: Text(
-                          'Sign up',
+                          'Login Here',
+                          style: TextStyle(
+                              color: Colors.indigo[200],
+                              fontWeight: FontWeight.w500,
+                              fontSize: 30),
+                        )),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'Sign in',
                           style: TextStyle(fontSize: 20),
+                        )),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'User Name',
                         ),
-                        onPressed: () {
-                          //signup screen
-                        },
-                      )
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.center,
-                ))
-              ],
-            )));
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      child: TextField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: (){
+                        //forgot password screen
+                      },
+                      textColor: Color(0xFF573E7A),
+                      child: Text('Forgot Password'),
+                    ),
+                    Container(
+                      height: 50,
+                        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Color(0xFF573E7A),
+                          child: Text('Login',
+                          style: TextStyle(fontSize: 15.0)),
+                          onPressed: () {
+                            print(nameController.text);
+                            print(passwordController.text);
+                          },
+                        )),
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          Text('Does not have account?'),
+                          FlatButton(
+                            textColor: Color(0xFF573E7A),
+                            child: Text(
+                              'Sign up',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            onPressed: () {
+                              //signup screen
+                            },
+                          )
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+                  ],
+                ))),
+        );
   }
 }
